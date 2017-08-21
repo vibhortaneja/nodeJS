65c58cd794ad1d875d782b1eb10966ed0d664238
vibhortaneja <vtaneja127@gmail.com>
2017-08-21 18:50:11 +0530

    Reducing Line

--- index.js
+++ index.js
@@ -4,70 +4,48 @@ var fs = require("fs");
 var lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('India2011.csv')
 });
-
 var myWriteStream = require("fs").createWriteStream("test_one.json")
-
 lineReader.on('line', function(line) {
     var jsonFromLine = {};
-
     var lineSplit = line.split(',');
-
     jsonFromLine.Total_Urban_Rural = lineSplit[4];
     jsonFromLine.AgeGroup = lineSplit[5];
     jsonFromLine.literate = lineSplit[12];
-
-
     result.push(jsonFromLine);
 });
-
 lineReader.on('close', function() {
     var i = 2;
     while (i != 30) {
         var loop = i.toString();
         jsonResult = result.filter(ele => ele['Total_Urban_Rural'] === "Total" && ele['AgeGroup'] === result[loop]['AgeGroup']);
-
         sum = jsonResult.reduce((s, ele) => {
             s = s + parseInt(ele.literate);
             return s;
         }, 0);
-
-
-
         var array = {};
         array.AgeGroup = result[loop]['AgeGroup'];
         array.totalLiterate = sum;
         final.push(array);
         i++;
-
     }
-
 });
-
 lineReader.on('close', function(line) {
     myWriteStream.write(JSON.stringify(final, null, 2))
 });
-
-
 /*----------------------2nd File------------------------------------------------*/
-
 var result_two = [];
 var final_two = [];
 var fs = require("fs");
-
 var lineReader_two = require('readline').createInterface({
     input: require('fs').createReadStream('India2011.csv')
 });
-
 var myWriteStream_two = require("fs").createWriteStream("test_two.json")
-
 lineReader_two.on('line', function(line) {
     var jsonFromLine_two = {};
-
     var lineSplit_two = line.split(',')
     var x = lineSplit_two[1]
     if (x == '01' || x == '02' || x == '03' || x == '04' || x == '05' || x == '06' || x == '07' || x == '08' || x == '09')
         x = x.charAt(1)
-
     jsonFromLine_two.StateCode = x
     jsonFromLine_two.StateName = lineSplit_two[3]
     jsonFromLine_two.Total_Urban_Rural = lineSplit_two[4]
@@ -75,19 +53,14 @@ lineReader_two.on('line', function(line) {
     jsonFromLine_two.Graduate = lineSplit_two[39]
     jsonFromLine_two.Graduate_Male = lineSplit_two[40]
     jsonFromLine_two.Graduate_Female = lineSplit_two[41]
-
     result_two.push(jsonFromLine_two)
-
 });
-
 var jsonResult_two = [];
 lineReader_two.on('close', function() {
     var j = 1;
     while (j != 36) {
         var loop = j.toString();
         jsonResult_two = result_two.filter(ele => ele['Total_Urban_Rural'] === "Total" && ele['AgeGroup'] === "All ages" && ele['StateCode'] === loop)
-
-
         var statename = jsonResult_two.map((obj) => obj.StateName)
         var graduate = jsonResult_two.map((obj) => obj.Graduate)
         var male = jsonResult_two.map((obj) => obj.Graduate_Male)
@@ -97,36 +70,24 @@ lineReader_two.on('close', function() {
         array_two.GraduateT = graduate[0]
         array_two.GraduateM = male[0]
         array_two.GraduateF = female[0]
-
         final_two.push(array_two)
         j++
-
     }
-
 });
-
 lineReader_two.on('close', function(line) {
     myWriteStream_two.write(JSON.stringify(final_two, null, 2))
 });
-
-
 /*-----------------3rd File--------------------------------------------------*/
-
 var result_three = [];
 var final_three = [];
 var fs = require("fs");
-
 var lineReader_three = require('readline').createInterface({
     input: require('fs').createReadStream('India2011.csv')
 });
-
 var myWriteStream_three = require("fs").createWriteStream("test_three.json")
-
 lineReader_three.on('line', function(line) {
     var jsonFromLine_three = {};
-
     var lineSplit_three = line.split(',')
-
     jsonFromLine_three.Total_Urban_Rural = lineSplit_three[4]
     jsonFromLine_three.AgeGroup = lineSplit_three[5]
     jsonFromLine_three.literate = lineSplit_three[15]
@@ -139,68 +100,51 @@ lineReader_three.on('line', function(line) {
     jsonFromLine_three.tech_diploma = lineSplit_three[36]
     jsonFromLine_three.graduate = lineSplit_three[40]
     jsonFromLine_three.unclassified = lineSplit_three[43]
-
-
     result_three.push(jsonFromLine_three)
-
 });
-
 var jsonResult_three = [];
 lineReader_three.on('close', function() {
-
     jsonResult_three = result_three.filter(ele => ele['Total_Urban_Rural'] === "Total" && ele['AgeGroup'] === "All ages")
-
     sumLiterate = jsonResult_three.reduce((s, ele) => {
         s = s + parseInt(ele.literate);
         return s;
     }, 0);
-
     sumBelowprimary = jsonResult_three.reduce((s, ele) => {
         s = s + parseInt(ele.belowprimary);
         return s;
     }, 0);
-
     sumPrimary = jsonResult_three.reduce((s, ele) => {
         s = s + parseInt(ele.primary);
         return s;
     }, 0);
-
     sumMiddle = jsonResult_three.reduce((s, ele) => {
         s = s + parseInt(ele.middle);
         return s;
     }, 0);
-
     sumSecondary = jsonResult_three.reduce((s, ele) => {
         s = s + parseInt(ele.secondary);
         return s;
     }, 0);
-
     sumHighersecondary = jsonResult_three.reduce((s, ele) => {
         s = s + parseInt(ele.higher_secondary);
         return s;
     }, 0);
-
     sumNondiploma = jsonResult_three.reduce((s, ele) => {
         s = s + parseInt(ele.non_diploma);
         return s;
     }, 0);
-
     sumTechdiploma = jsonResult_three.reduce((s, ele) => {
         s = s + parseInt(ele.tech_diploma);
         return s;
     }, 0);
-
     sumGraduate = jsonResult_three.reduce((s, ele) => {
         s = s + parseInt(ele.graduate);
         return s;
     }, 0);
-
     sumUnclassified = jsonResult_three.reduce((s, ele) => {
         s = s + parseInt(ele.unclassified);
         return s;
     }, 0);
-
-
     var array_three = {};
     array_three.Literate_without_education = sumLiterate
     array_three.Below_primary = sumBelowprimary
@@ -212,13 +156,8 @@ lineReader_three.on('close', function() {
     array_three.Tech_diploma = sumTechdiploma
     array_three.Graduate = sumGraduate
     array_three.Unclassified = sumUnclassified
-
-
     final_three.push(array_three)
-
-
 });
-
 lineReader_three.on('close', function(line) {
     myWriteStream_three.write(JSON.stringify(final_three, null, 2))
 });
