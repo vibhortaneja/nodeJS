42f379a7ff8d503d6977dad3cf5d7cff78677217
vibhortaneja <vtaneja127@gmail.com>
2017-08-22 14:45:28 +0530

    Optimised code

--- Modules/index3.js
+++ Modules/index3.js
@@ -1,6 +1,6 @@
 var final_three = [];
 var fs = require("fs");
-  var s = [0,0,0,0,0,0,0,0,0,0]
+var s = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 var lineReader_three = require('readline').createInterface({
     input: require('fs').createReadStream('India2011.csv')
 });
@@ -12,78 +12,76 @@ lineReader_three.on('line', function(line) {
 
     var lineSplit_three = line.split(',')
 
-if(lineSplit_three[4]=='Total' && lineSplit_three[5]=='All ages')
-{
-for(let i=0,j=15;i<10;i++,j+=3)
-{
-	s[i]=s[i]+parseInt(lineSplit_three[j])
-}    
-}}
-);
+    if (lineSplit_three[4] == 'Total' && lineSplit_three[5] == 'All ages') {
+        for (let i = 0, j = 15; i < 10; i++, j += 3) {
+            s[i] = s[i] + parseInt(lineSplit_three[j])
+        }
+    }
+});
 
 lineReader_three.on('close', function() {
-     		obj = {
-    			literate: s[0],
-    			belowprimary: s[1],
-    			primary: s[2],
-    			middle: s[3],
-    			secondary: s[4],
-    			higher_secondary: s[5],
-    			non_diploma: s[6],
-    			tech_diploma: s[7],
-    			graduate: s[8],
-    			unclassified: s[9]
-    		}
-    		final_three.push(obj)
-       myWriteStream_three.write(JSON.stringify(final_three, null, 2)) 
+    obj = {
+        literate: s[0],
+        belowprimary: s[1],
+        primary: s[2],
+        middle: s[3],
+        secondary: s[4],
+        higher_secondary: s[5],
+        non_diploma: s[6],
+        tech_diploma: s[7],
+        graduate: s[8],
+        unclassified: s[9]
+    }
+    final_three.push(obj)
+    myWriteStream_three.write(JSON.stringify(final_three, null, 2))
 })
-      /*  sumBelowprimary = jsonResult_three.reduce((s, ele) => {
-            s = s + parseInt(ele.belowprimary);
-            return s;
-        }, 0);
+/*  sumBelowprimary = jsonResult_three.reduce((s, ele) => {
+      s = s + parseInt(ele.belowprimary);
+      return s;
+  }, 0);
 
-        sumPrimary = jsonResult_three.reduce((s, ele) => {
-            s = s + parseInt(ele.primary);
-            return s;
-        }, 0);
+  sumPrimary = jsonResult_three.reduce((s, ele) => {
+      s = s + parseInt(ele.primary);
+      return s;
+  }, 0);
 
-        sumMiddle = jsonResult_three.reduce((s, ele) => {
-            s = s + parseInt(ele.middle);
-            return s;
-        }, 0);
+  sumMiddle = jsonResult_three.reduce((s, ele) => {
+      s = s + parseInt(ele.middle);
+      return s;
+  }, 0);
 
-        sumSecondary = jsonResult_three.reduce((s, ele) => {
-            s = s + parseInt(ele.secondary);
-            return s;
-        }, 0);
+  sumSecondary = jsonResult_three.reduce((s, ele) => {
+      s = s + parseInt(ele.secondary);
+      return s;
+  }, 0);
 
-        sumHighersecondary = jsonResult_three.reduce((s, ele) => {
-            s = s + parseInt(ele.higher_secondary);
-            return s;
-        }, 0);
+  sumHighersecondary = jsonResult_three.reduce((s, ele) => {
+      s = s + parseInt(ele.higher_secondary);
+      return s;
+  }, 0);
 
-        sumNondiploma = jsonResult_three.reduce((s, ele) => {
-            s = s + parseInt(ele.non_diploma);
-            return s;
-        }, 0);
+  sumNondiploma = jsonResult_three.reduce((s, ele) => {
+      s = s + parseInt(ele.non_diploma);
+      return s;
+  }, 0);
 
-        sumTechdiploma = jsonResult_three.reduce((s, ele) => {
-            s = s + parseInt(ele.tech_diploma);
-            return s;
-        }, 0);
+  sumTechdiploma = jsonResult_three.reduce((s, ele) => {
+      s = s + parseInt(ele.tech_diploma);
+      return s;
+  }, 0);
 
-        sumGraduate = jsonResult_three.reduce((s, ele) => {
-            s = s + parseInt(ele.graduate);
-            return s;
-        }, 0);
+  sumGraduate = jsonResult_three.reduce((s, ele) => {
+      s = s + parseInt(ele.graduate);
+      return s;
+  }, 0);
 
-        sumUnclassified = jsonResult_three.reduce((s, ele) => {
-            s = s + parseInt(ele.unclassified);
-            return s;
-        }, 0);*/
+  sumUnclassified = jsonResult_three.reduce((s, ele) => {
+      s = s + parseInt(ele.unclassified);
+      return s;
+  }, 0);*/
 
 
-    /*    var array_three = {};
+/*    var array_three = {};
         array_three.Literate_without_education = sumLiterate
         array_three.Below_primary = sumBelowprimary
         array_three.primary = sumPrimary
--- index.js
+++ index.js
@@ -78,9 +78,9 @@ lineReader_two.on('close', function(line) {
     myWriteStream_two.write(JSON.stringify(final_two, null, 2))
 });
 /*-----------------3rd File--------------------------------------------------*/
-var result_three = [];
 var final_three = [];
 var fs = require("fs");
+var s = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 var lineReader_three = require('readline').createInterface({
     input: require('fs').createReadStream('India2011.csv')
 });
@@ -88,76 +88,25 @@ var myWriteStream_three = require("fs").createWriteStream("test_three.json")
 lineReader_three.on('line', function(line) {
     var jsonFromLine_three = {};
     var lineSplit_three = line.split(',')
-    jsonFromLine_three.Total_Urban_Rural = lineSplit_three[4]
-    jsonFromLine_three.AgeGroup = lineSplit_three[5]
-    jsonFromLine_three.literate = lineSplit_three[15]
-    jsonFromLine_three.belowprimary = lineSplit_three[18]
-    jsonFromLine_three.primary = lineSplit_three[21]
-    jsonFromLine_three.middle = lineSplit_three[24]
-    jsonFromLine_three.secondary = lineSplit_three[27]
-    jsonFromLine_three.higher_secondary = lineSplit_three[30]
-    jsonFromLine_three.non_diploma = lineSplit_three[33]
-    jsonFromLine_three.tech_diploma = lineSplit_three[36]
-    jsonFromLine_three.graduate = lineSplit_three[40]
-    jsonFromLine_three.unclassified = lineSplit_three[43]
-    result_three.push(jsonFromLine_three)
+    if (lineSplit_three[4] == 'Total' && lineSplit_three[5] == 'All ages') {
+        for (let i = 0, j = 15; i < 10; i++, j += 3) {
+            s[i] = s[i] + parseInt(lineSplit_three[j])
+        }
+    }
 });
-var jsonResult_three = [];
 lineReader_three.on('close', function() {
-    jsonResult_three = result_three.filter(ele => ele['Total_Urban_Rural'] === "Total" && ele['AgeGroup'] === "All ages")
-    sumLiterate = jsonResult_three.reduce((s, ele) => {
-        s = s + parseInt(ele.literate);
-        return s;
-    }, 0);
-    sumBelowprimary = jsonResult_three.reduce((s, ele) => {
-        s = s + parseInt(ele.belowprimary);
-        return s;
-    }, 0);
-    sumPrimary = jsonResult_three.reduce((s, ele) => {
-        s = s + parseInt(ele.primary);
-        return s;
-    }, 0);
-    sumMiddle = jsonResult_three.reduce((s, ele) => {
-        s = s + parseInt(ele.middle);
-        return s;
-    }, 0);
-    sumSecondary = jsonResult_three.reduce((s, ele) => {
-        s = s + parseInt(ele.secondary);
-        return s;
-    }, 0);
-    sumHighersecondary = jsonResult_three.reduce((s, ele) => {
-        s = s + parseInt(ele.higher_secondary);
-        return s;
-    }, 0);
-    sumNondiploma = jsonResult_three.reduce((s, ele) => {
-        s = s + parseInt(ele.non_diploma);
-        return s;
-    }, 0);
-    sumTechdiploma = jsonResult_three.reduce((s, ele) => {
-        s = s + parseInt(ele.tech_diploma);
-        return s;
-    }, 0);
-    sumGraduate = jsonResult_three.reduce((s, ele) => {
-        s = s + parseInt(ele.graduate);
-        return s;
-    }, 0);
-    sumUnclassified = jsonResult_three.reduce((s, ele) => {
-        s = s + parseInt(ele.unclassified);
-        return s;
-    }, 0);
-    var array_three = {};
-    array_three.Literate_without_education = sumLiterate
-    array_three.Below_primary = sumBelowprimary
-    array_three.Primary = sumPrimary
-    array_three.Middle = sumMiddle
-    array_three.Secondary = sumSecondary
-    array_three.Higher_secondary = sumHighersecondary
-    array_three.Non_diploma = sumNondiploma
-    array_three.Tech_diploma = sumTechdiploma
-    array_three.Graduate = sumGraduate
-    array_three.Unclassified = sumUnclassified
-    final_three.push(array_three)
-});
-lineReader_three.on('close', function(line) {
+    obj = {
+        literate: s[0],
+        belowprimary: s[1],
+        primary: s[2],
+        middle: s[3],
+        secondary: s[4],
+        higher_secondary: s[5],
+        non_diploma: s[6],
+        tech_diploma: s[7],
+        graduate: s[8],
+        unclassified: s[9]
+    }
+    final_three.push(obj)
     myWriteStream_three.write(JSON.stringify(final_three, null, 2))
-});
+})
